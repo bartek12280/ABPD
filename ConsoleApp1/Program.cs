2bd66f7afc01c69a26652c4ff6d01d84119cfa96
Bartek <bartek.karas12280@gmail.com>
2024-03-14 23:21:16 +0100

    add if statement

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,10 +4,22 @@ public class Program
 {
     static void Main(string[] args)
     {
+        // variables
         int age = 25;
         float weight = 65.5f;
         double height = 193.5;
         string name = "Bartek";
         bool isStudent = true;
+        
+        
+        // if statements
+        if (age > 18)
+        {
+            Console.WriteLine("Adult");
+        }
+        else
+        {
+            Console.WriteLine("under 18!!!");
+        }
     }
 }
